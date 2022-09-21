20b70e4864df21cbd49ccb17966858d2ac6cbba1
CIT142F-43@m307.uc-bcf.edu.ph <CIT142F-43@m307.uc-bcf.edu.ph>
2022-09-21 17:47:07 +0800

    batch upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/echotamondong/filemwo-echo.git
+git remote set-url origin https://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@github.com/echotamondong/filemwo-echo.git
 pause
 git add .
 git commit -m "batch upload"
